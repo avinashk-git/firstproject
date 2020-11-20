165f9dafcf3017b3dd0c6884887aae01888998b6
kolaa <avinash.kolavennu@in.pega.com>
2020-11-20 18:34:06 +0100

    testing commit in github

---
+++ Automation/src/com/automation/FileOperation.java
@@ -0,0 +1,19 @@
+package com.automation;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FileOperation {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+
+		String fileContent= new String();
+		
+		fileContent="avinash";
+		
+		System.out.println(fileContent);
+	}
+
+	
+}
